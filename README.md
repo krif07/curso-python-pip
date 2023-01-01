057761afe67604528e95e5f2dc5307fb45d344ff
Cristian Fernando Dávila López <krif07@gmail.com>
2022-12-31 20:50:14 -0500

    Se adiciona fastapi para crear un server fake y uvicorn como servidor de apps

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ source env/bin/activate
 pip3 install -r requirements.txt
 python3 main.py
 ```
+
+# web-server Project
+
+```sh
+git clone
+cd web-server
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+uvicorn main:app --reload
+ir a 
+http://localhost:8000/
+http://localhost:8000/contact
+```
--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,24 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3, 4, 5]
+
+@app.get('/resources')
+def get_resources():
+    return {'Empresa': 'Platzi'}
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Página de contacto</h1>
+        <p>Cristian Dávila</p>
+        <p>krif07@gmail.com</p>
+    """
 
 def run():
     store.get_categories()
